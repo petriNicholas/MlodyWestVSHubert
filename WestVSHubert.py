3c85ec48b9f2af1c8c13c30ef05b80700e56a165
petriNicholas <93609490+petriNicholas@users.noreply.github.com>
2024-09-30 17:19:27 +0200

    updated xlsx and adding new rows to 'database'

--- WestVSHubert.py
+++ WestVSHubert.py
@@ -1,4 +1,6 @@
 import requests
+import time
+import openpyxl
 from bs4 import BeautifulSoup
 
 hubert = requests.get("https://open.spotify.com/artist/6Vhpr2gUnE0VcGjzqDmM62")
@@ -16,3 +18,13 @@ westListeners = int(soupW.get_text().split("West")[2].split(" monthly listeners"
 print("Hubert. :	", hubertListeners)
 print("West: 	", westListeners)
 
+diffListeners = westListeners - hubertListeners
+
+wb = openpyxl.load_workbook('WestVSHubertBAZA.xlsx')
+ws = wb.active
+
+new_row = (ws.max_row+2137, time.gmtime().tm_year, time.gmtime().tm_mon, time.gmtime().tm_mday, westListeners, hubertListeners, diffListeners)
+
+ws.append(new_row)
+
+wb.save('WestVSHubertBAZA.xlsx')
